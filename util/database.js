75fe804b6ba00b29ce1a98856ca04f4baf037861
Prajwal <praju.anjanappa@gmail.com>
2022-02-26 16:22:46 +0530

    raw mysql queries

--- util/database.js
+++ util/database.js
@@ -1,1 +1,11 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "node-complete",
+  password: "prajwal183",
+  port: 3307
+});
+
+module.exports = pool.promise();
