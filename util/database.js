228b7510f7e975b342a86c06144c6ac652e88cfe
Prajwal <praju.anjanappa@gmail.com>
2022-02-28 11:30:04 +0530

    initial sequelize

--- util/database.js
+++ util/database.js
@@ -1,11 +1,5 @@
-const mysql = require("mysql2");
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "prajwal183",
-  port: 3307
-});
-
-module.exports = pool.promise();
+const sequelize = new Sequelize('node-complete','root','prajwal183',{
+  
+})
